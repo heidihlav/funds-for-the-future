10bf457cf708527f0d083cda16b2166a9b94a08b
Heidi Hlavinka <hchlav@gmail.com>
2023-02-06 16:30:00 -0600

    Created post route for users sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController 
 
     get '/users/new' do 
-      erb :'/users/new'
+        erb :'/users/new'
     end 
   
-    post '/users' do 
-  
+    post '/users' do
+        @user = User.new(email: params[:email], password: params[:password])
+        if @user.save
+            session[:id] = @user.id
+            redirect '/'
+        else
+            erb :'users/new'
+        end
     end
+
+
+
+
+
+
   end
