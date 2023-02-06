a3011715f43a6f43554f86d020c74a0fcc97ef34
Heidi Hlavinka <hchlav@gmail.com>
2023-02-06 16:58:41 -0600

    Add user authentication for login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,17 @@ class SessionsController < ApplicationController
     end
 
     post '/login' do
-
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect '/'
+        else 
+            @error = "Incorrect email or password."
+            erb :'sessions/login'
+        end
     end
 
+
+    
 
 end
