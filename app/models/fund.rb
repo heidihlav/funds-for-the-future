09bdd222951bc2f469285320a342e262f4dc0995
Heidi Hlavinka <hchlav@gmail.com>
2023-02-13 17:53:20 -0600

    Recreated funds table to test validations.

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,4 +1,6 @@
 class Fund < ActiveRecord::Base
     belongs_to :user
+    validates :name, presence: true, uniqueness: true
+    validates :age, presence: true
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_182538) do
+ActiveRecord::Schema.define(version: 2023_02_13_235133) do
 
   create_table "funds", force: :cascade do |t|
     t.string "name"
